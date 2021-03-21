b214e80897cd48e63696c68c7b7283dbe190b29e
Robert <rmartin-short@berkeley.edu>
2021-03-21 16:30:28 -0700

    added to __init__ in top level package dir

--- generic_search/__init__.py
+++ generic_search/__init__.py
@@ -3,4 +3,9 @@ generic_search
 ===============
 A package for creating customizable search engines to run on a supplied corpus
 """
+
+__author__ = """Robert Martin-Short"""
+__email__ = 'martinshortr@gmail.com'
+__version__ = '0.1.0'
+
 from generic_search.engine.search_engine import SearchEngine
